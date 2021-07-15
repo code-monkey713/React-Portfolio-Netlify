3dce58fe2a7961e75a9a06b6e0b2758d307a1edb
Tony Tran <codemonkey713@gmail.com>
2021-07-15 18:15:03 -0500

    added main wrapper

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import LandingPage from '../pages/LandingPage';
+import AboutMe from '../pages/AboutMe';
+import Contact from '../pages/Contact';
+import Projects from '../pages/Projects';
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+    <Route path="/aboutme" component={AboutMe} />
+    <Route path="/contact" component={Contact} />
+    <Route path="/projects" component={Projects} />
+  </Switch>
+);
+
+export default Main;
