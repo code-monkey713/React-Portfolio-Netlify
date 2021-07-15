f788bf9150e2ef5276829dd453ce9471425fc71a
Tony Tran <codemonkey713@gmail.com>
2021-07-15 18:12:27 -0500

    renaming footer to footnote due to naming issue

---
+++ src/components/FootNote.js
@@ -0,0 +1,18 @@
+// import { Footer, FooterSection, FooterLinkList } from 'react-mdl';
+
+const FootNote = () => {
+  return (
+    <div className="row footer-color">
+      <div className="col">💛 Tony Tran 💛</div>
+    </div>
+    // <Footer size="mini">
+    //   <FooterSection type="left" logo="Title">
+    //     <FooterLinkList>
+    //       <div className="col">💛 Tony Tran 💛</div>
+    //     </FooterLinkList>
+    //   </FooterSection>
+    // </Footer>
+  );
+};
+
+export default FootNote;
