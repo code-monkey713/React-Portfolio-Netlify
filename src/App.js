1dd4de7434f9400078149c0964c69245d8f3dbaf
Tony Tran <codemonkey713@gmail.com>
2021-07-15 18:18:15 -0500

    added framework for the portfolio on app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar';
+import FootNote from './components/FootNote';
+
+import LandingPage from './pages/LandingPage';
+import AboutMe from './pages/AboutMe';
+import Projects from './pages/Projects';
+import Contact from './pages/Contact';
+
+import { Layout } from 'react-mdl';
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className="demo-big-content">
+      <Layout>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact>
+            <LandingPage />
+          </Route>
+          <Route path="/aboutme">
+            <AboutMe />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+        <FootNote />
+      </Layout>
     </div>
   );
 }
