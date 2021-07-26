56fb48df9b230b20cd9c6d2abfe9f094daaa9502
Tony Tran <codemonkey713@gmail.com>
2021-07-26 16:18:38 -0500

    added design library

---
+++ backup/design/library/index.js
@@ -0,0 +1,6 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faSun);
+library.add(faMoon);
+// library.add();
